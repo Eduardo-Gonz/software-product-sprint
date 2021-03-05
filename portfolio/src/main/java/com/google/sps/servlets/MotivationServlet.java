944203fe637799dacc5a4d2f48f22db0a715af12
Eduardo <erugonz249@gmail.com>
2021-03-05 19:20:26 +0000

    Add button to index.html to ouput the content retrieved from motivation.js

--- portfolio/src/main/java/com/google/sps/servlets/MotivationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MotivationServlet.java
@@ -6,13 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
+/** Handles requests sent to the /motivation URL. Try running a server and navigating to /motivation! */
 @WebServlet("/motivation")
 public class MotivationServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>With no pain, there is no gain</h1>");
+    response.getWriter().println("With no pain, there is no gain");
   }
 }
